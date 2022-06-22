73c44380c3219e7f786a447d34577a9bc2c0d7c4
2yunseong <dbsdltjd3701@naver.com>
2022-06-22 22:57:25 +0900

    feat : 도로명 주소 불러오기 ( use State)

--- src/Register/PostCode.js
+++ src/Register/PostCode.js
@@ -1,42 +1,37 @@
-import axios from "axios";
-import React from "react";
+import React, { useState } from "react";
 import { useDaumPostcodePopup } from "react-daum-postcode";
 
 const PostCode = () => {
   const scriptUrl =
     "https://t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js";
   const open = useDaumPostcodePopup(scriptUrl);
+  const [roadAddress, setRoadAddress] = useState("도로명 주소");
+  const [buildingName, setBuildingName] = useState("빌딩 이름");
 
   const handleComplete = (data) => {
-    let fullAddress = data.address;
-    let extraAddress = "";
+    setRoadAddress(data.roadAddress);
 
     if (data.addressType === "R") {
-      if (data.bname !== "") {
-        extraAddress += data.bname;
-      }
       if (data.buildingName !== "") {
-        extraAddress +=
-          extraAddress !== "" ? `, ${data.buildingName}` : data.buildingName;
+        setBuildingName(data.buildingName);
       }
-      fullAddress += extraAddress !== "" ? ` (${extraAddress})` : "";
     }
 
-    console.log(fullAddress); // e.g. '서울 성동구 왕십리로2길 20 (성수동1가)'
+    console.log(buildingName, roadAddress);
   };
 
   const handleClick = () => {
     open({ onComplete: handleComplete });
   };
 
-  const transformAddress = () => {
-    axios.get();
-  };
-
   return (
-    <button type='button' onClick={handleClick}>
-      Open
-    </button>
+    <div>
+      <p>도로명주소 : {roadAddress}</p>
+      <p>도착지 : {buildingName} </p>
+      <button type='button' onClick={handleClick}>
+        Open
+      </button>
+    </div>
   );
 };
 
