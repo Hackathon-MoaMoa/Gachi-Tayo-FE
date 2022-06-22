0f61b4a3ee3975cb2269ab575061373dfd102548
2yunseong <dbsdltjd3701@naver.com>
2022-06-23 06:02:50 +0900

    feat : 좌석 선택 로직 구현

--- src/Register/Seat.js
+++ src/Register/Seat.js
@@ -1,26 +1,98 @@
 import { useState } from "react";
 import "./Seat.css";
 import SeatDisable from "./img/seat_disable.png";
+import SeatCheck from "./img/check_seat.png";
 import Seat1 from "./img/seat1.png";
 import Seat2 from "./img/seat2.png";
 import Seat3 from "./img/seat3.png";
 import Seat4 from "./img/seat4.png";
 
-const Seat = (props) => {
+const Seat = () => {
+  const [checkSeat, setCheckSeat] = useState(0);
+
+  const onCheck = (e) => {
+    setCheckSeat(parseInt(e.target.alt));
+  };
+
+  const onUnCheck = () => {
+    setCheckSeat(0);
+  };
+
   return (
     <div className='seat'>
       <div className='front-seat'>
         <img className='seat-img' src={SeatDisable} alt='driver' />
-        <img className='seat-img' src={Seat1} alt='1' />
+        {checkSeat === 1 ? (
+          <img
+            key='1'
+            onClick={onUnCheck}
+            className='seat-img'
+            src={SeatCheck}
+            alt='1'
+          />
+        ) : (
+          <img
+            key='1'
+            onClick={onCheck}
+            className='seat-img'
+            src={Seat1}
+            alt='1'
+          />
+        )}
       </div>
       <div className='back-seat'>
-        <img className='seat-img' src={Seat2} alt='2' />
-        <img className='seat-img' src={Seat3} alt='3' />
-        <img className='seat-img' src={Seat4} alt='4' />
+        {checkSeat === 2 ? (
+          <img
+            key='2'
+            onClick={onUnCheck}
+            className='seat-img'
+            src={SeatCheck}
+            alt='2'
+          />
+        ) : (
+          <img
+            key='2'
+            onClick={onCheck}
+            className='seat-img'
+            src={Seat2}
+            alt='2'
+          />
+        )}
+        {checkSeat === 3 ? (
+          <img
+            key='3'
+            onClick={onUnCheck}
+            className='seat-img'
+            src={SeatCheck}
+            alt='3'
+          />
+        ) : (
+          <img
+            key='3'
+            onClick={onCheck}
+            className='seat-img'
+            src={Seat3}
+            alt='3'
+          />
+        )}
+        {checkSeat === 4 ? (
+          <img
+            key='4'
+            onClick={onUnCheck}
+            className='seat-img'
+            src={SeatCheck}
+            alt='4'
+          />
+        ) : (
+          <img
+            key='4'
+            onClick={onCheck}
+            className='seat-img'
+            src={Seat4}
+            alt='4'
+          />
+        )}
       </div>
-
-      {/*
-       */}
     </div>
   );
 };
