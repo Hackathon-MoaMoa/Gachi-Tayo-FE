f463900261cf3fa6d04ce831a6aa1bfaf184d9b9
2yunseong <dbsdltjd3701@naver.com>
2022-06-22 22:27:31 +0900

    feat : 도로명 주소 입력

---
+++ src/Register/PostCode.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+import React from "react";
+import { useDaumPostcodePopup } from "react-daum-postcode";
+
+const PostCode = () => {
+  const scriptUrl =
+    "https://t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js";
+  const open = useDaumPostcodePopup(scriptUrl);
+
+  const handleComplete = (data) => {
+    let fullAddress = data.address;
+    let extraAddress = "";
+
+    if (data.addressType === "R") {
+      if (data.bname !== "") {
+        extraAddress += data.bname;
+      }
+      if (data.buildingName !== "") {
+        extraAddress +=
+          extraAddress !== "" ? `, ${data.buildingName}` : data.buildingName;
+      }
+      fullAddress += extraAddress !== "" ? ` (${extraAddress})` : "";
+    }
+
+    console.log(fullAddress); // e.g. '서울 성동구 왕십리로2길 20 (성수동1가)'
+  };
+
+  const handleClick = () => {
+    open({ onComplete: handleComplete });
+  };
+
+  const transformAddress = () => {
+    axios.get();
+  };
+
+  return (
+    <button type='button' onClick={handleClick}>
+      Open
+    </button>
+  );
+};
+
+export default PostCode;
--- src/Register/Register.js
+++ src/Register/Register.js
@@ -1,5 +1,19 @@
+import PostCode from "./PostCode";
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <div className='register'>
+      <div>출발지</div>
+      <div>
+        <PostCode />
+      </div>
+      <div>도착지</div>
+      <div>
+        <PostCode />
+      </div>
+      <div>출발시간</div>
+      <div>좌석</div>
+    </div>
+  );
 };
 
 export default Register;
