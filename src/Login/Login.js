e7ecd9e2b7617f5aef0bf0ac4aeb65537c931572
2yunseong <dbsdltjd3701@naver.com>
2022-06-23 04:23:53 +0900

    feat : Login 스켈레톤 코드

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Logo from "./img/blue-logo.png";
 
 const Login = () => {
   return (
     <div>
-      <input className='sign-in-id' placeholder='아이디' />
-      <input className='sign-in-passwd' placeholder='비밀번호' />
+      <img src={Logo} alt='logo' />
+      <input className='sign-in-id' placeholder='아이디' /> <br />
+      <input className='sign-in-passwd' placeholder='비밀번호' /> <br />
       <button className='login-button'>로그인</button>
       {/* 회원가입 으로 링크되는 버튼 */}
     </div>
