dec5ba767618597314607977b55fad474e75ef5d
2yunseong <dbsdltjd3701@naver.com>
2022-06-24 07:10:44 +0900

    feat : 메인 페이지 디자인 적용

--- src/Main/MainPage.js
+++ src/Main/MainPage.js
@@ -1,5 +1,14 @@
+import Background from "./img/background.png";
+import Slogan from "./img/slogan.png";
+import "./MainPage.css";
+
 const MainPage = () => {
-  return <div>main page</div>;
+  return (
+    <div>
+      <img className='main-slogan' src={Slogan} alt='slogan' />
+      <img className='main-background' src={Background} alt='background' />
+    </div>
+  );
 };
 
 export default MainPage;
