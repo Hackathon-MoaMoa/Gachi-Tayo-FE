eb6cb48a74f2762b446e865f853574bb2cab6dcd
2yunseong <dbsdltjd3701@naver.com>
2022-06-24 03:45:21 +0900

    feat : api 5번 axios 통신

--- src/Reservation/Reservation.js
+++ src/Reservation/Reservation.js
@@ -1,8 +1,25 @@
+import axios from "axios";
 import ReservationSearch from "./ReservationSearch";
 import ReservationList from "./ReservationList";
 import "./Reservation.css";
+import { useEffect, useState } from "react";
 
 const Reservation = () => {
+  const [requestArray, setRequestArray] = useState([]);
+
+  useEffect(() => {
+    axios({
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+      method: "get",
+      url: "https://gachi-tayo.shop/api/posts",
+      responseType: "json",
+    }).then((response) => {
+      console.log(response.data);
+      setRequestArray(response.data);
+    });
+  }, []);
   return (
     <div className='reservation'>
       <ReservationSearch />
