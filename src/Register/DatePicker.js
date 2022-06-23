2a8db25cb7f508af3298879b28b2039a205faf74
2yunseong <dbsdltjd3701@naver.com>
2022-06-23 18:21:59 +0900

    feat : Date Picker Dto

--- src/Register/DatePicker.js
+++ src/Register/DatePicker.js
@@ -1,25 +1,47 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./DatePicker.css";
 
 const SERVER_URL = "http://localhost:3000";
 
 const DatePicker = () => {
+  const [year, setYear] = useState("2022");
+  const [month, setMonth] = useState("06");
+  const [day, setDay] = useState("22");
+  const [hour, setHour] = useState("18");
+  const [min, setMin] = useState("30");
+  const [dateFormat, setDateFormat] = useState("");
+
   const iframePart = () => {
     return {
       __html: `<iframe src="/TimePicker/timepicker.html" width="600px" height="440px" frameborder=0 framespacing=0 marginheight=0 marginwidth=0 scrolling=no vspace=0 style='margin-bottom="500px"'></iframe>`,
     };
   };
+
   useEffect(() => {
     window.addEventListener(
       "message",
       (e) => {
         if (e.origin === SERVER_URL && e.data.message) {
-          console.log(e.data.message);
+          console.dir(e.data.message);
+          const y = e.data.message.getFullYear();
+          const mon = e.data.message.getMonth() + 1;
+          const d = e.data.message.getUTCDate();
+          const h = e.data.message.getHours();
+          const m = e.data.message.getMinutes();
+          console.log(y, mon, d, h, m);
+          setYear(y);
+          setMonth(mon);
+          setDay(d);
+          setHour(h);
+          setMin(m);
+          const strformat = `${year}-${month}-${day} ${hour}:${min}`;
+          setDateFormat(dateFormat);
         }
       },
       false
     );
   }, []);
+
   return (
     <div
       style={{
